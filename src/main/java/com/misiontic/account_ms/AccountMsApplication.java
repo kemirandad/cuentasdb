ddbf6631748db1372d17ade24f60bd3af67e408a
kemirandad <kjmd2s@gmail.com>
2021-08-11 13:36:39 -0500

    Microservicio terminado

--- src/main/java/com/misiontic/account_ms/AccountMsApplication.java
+++ src/main/java/com/misiontic/account_ms/AccountMsApplication.java
@@ -2,12 +2,23 @@ package com.misiontic.account_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+
+@RestController
 public class AccountMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMsApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello(){
+		return List.of("Hola, mundo, Salut, tout le monde, Hello, World!");
 	}
 
 }
